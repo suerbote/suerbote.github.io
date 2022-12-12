b8b6cc3d21aeda4b29c462a3b5e1af94fd86a285
suerbote <111413795+suerbote@users.noreply.github.com>
2022-12-12 22:57:40 +0800

    Add files via upload

---
+++ one.js
@@ -0,0 +1,16 @@
+var body = document.getElementById('body');
+// JavaScript修改样式
+function Font_deh() {
+    body.style.fontFamily = "'deyihei', consolas, 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif";
+
+};
+function Font_song() {
+    body.style.fontFamily = "'Han', serif, consolas, 'Segoe UI', Tahoma, Geneva, Verdana,sans-serif";
+};
+function Font_har() {
+    body.style.fontFamily = "'harmory', serif, consolas, 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif";
+};
+function Font_none() {
+    body.style.fontFamily = "微软雅黑";
+    body.style.fontWeight = "500";
+};
